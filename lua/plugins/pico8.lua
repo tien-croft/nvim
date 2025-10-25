3383fd65df48802898733775123ed2c1f065d5d9
Tien Trinh <tqtien@croft-ai.com>
2025-10-25 17:40:06 +0900

    feat: add pico-8 lsp

---
+++ lua/plugins/pico8.lua
@@ -0,0 +1,31 @@
+local configs = require("lspconfig.configs")
+
+if not configs.pico8_ls then
+  configs.pico8_ls = {
+    default_config = {
+      cmd = { "pico8-ls", "--stdio" },
+      filetypes = { "pico8", "p8" },
+      root_dir = function(fname)
+        return vim.fs.dirname(fname)
+      end,
+      settings = {},
+    },
+  }
+end
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pico8_ls = {}, -- enable it
+      },
+      setup = {
+        pico8_ls = function(_, opts)
+          require("lspconfig").pico8_ls.setup(opts)
+          return true
+        end,
+      },
+    },
+  },
+}
